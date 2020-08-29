4c7352b4d9ba025e25ae1192b536c9a262957344
Eduard <eduard.duarte@hotmail.com>
2020-08-28 19:51:51 -0500

    ordered

--- Protractor/e2e/src/app.po.ts
+++ Protractor/e2e/src/app.po.ts
@@ -1,13 +1,11 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  navigateTo(): Promise<unknown> {
-    console.log("entro a navigateTo");
-    return browser.get(browser.baseUrl) as Promise<unknown>;
+  navigateTo() {
+    return browser.get(browser.baseUrl) as Promise<any>;
   }
 
-  getTitleText(): Promise<string> {
-    console.log("entro a getTitleText");
+  getTitleText() {
     return element(by.css('app-root h1')).getText() as Promise<string>;
   }
 }
